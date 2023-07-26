7a28c11fb99aaec56520169e8996fa36a8b42666
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-07-27 01:16:23 +0700

    feat: add auto base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/healthydoc/';
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	$uri = 'https://';
+} else {
+	$uri = 'http://';
+}
+$root = $uri . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url']    = "$root";
 
 /*
 |--------------------------------------------------------------------------
